305293b34fa57491217e89bfb8c1ba645fa8222c
jtranberg <jtranberg@hotmail.ca>
2025-04-21 22:05:13 -0700

    Include JSON model config files in backend

--- release.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = "https://github.com/jtranberg/8_class_model/releases/download/v1.0/best_model.keras"
-response = requests.get(url, allow_redirects=True)
-
-print("Status:", response.status_code)
-print("Content-Type:", response.headers.get("Content-Type"))
-print("Length:", len(response.content))
