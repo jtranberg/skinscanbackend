6f4b04afcdfef139eabc4fc23eca49209e30e962
jtranberg <jtranberg@hotmail.ca>
2025-04-22 01:16:50 -0700

    Include JSON model config files in backend

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ CORS(app)
 
 # === MongoDB Setup ===
 MONGO_URI = os.getenv("MONGO_URI") or "mongodb://localhost:27017"
-client = MongoClient(MONGO_URI, tlsCAFile=certifi.where())
+client = MongoClient(MONGO_URI, tls=True, tlsCAFile=certifi.where())
 db = client['epidermus']
 users_collection = db['users']
 
